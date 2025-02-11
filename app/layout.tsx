1c271daffc7db2f0fbe9e5d456159e7813bef068
Leeho Lim <leeholim21@gmail.com>
2025-02-11 17:39:43 +0000

    Add white background

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       {/* suppressHydrationWarning: https://github.com/vercel/next.js/issues/44343 */}
       <body
-        className={`${inter.variable} ${inter_tight.variable} font-inter antialiased bg-indigo-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200 tracking-tight`}
+        className={`${inter.variable} ${inter_tight.variable} font-inter antialiased text-gray-800 dark:bg-gray-900 dark:text-gray-200 tracking-tight`}
       >
         <Theme>
           <div className="relative flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
